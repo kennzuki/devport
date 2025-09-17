00dae7a5514932fe909c71ad9eb8ed83466902c5
kennzuki <kingsyken@gmail.com>
2025-09-17 18:16:20 +0300

    fetching data wit loaders

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,4 +1,18 @@
-const ProjectsPage = () => {
+import type { ComponentProps } from "react";
+import type { Route } from "./+types/index";
+
+export async function loader({request}: Route.LoaderArgs):Promise<any> {
+  const res = await fetch('https://localhost:8000/projects')
+  const data= await res.json()
+  return {projects:data}
+}
+
+
+const ProjectsPage = ({ loaderData }: Route.ComponentProps) => {
+  
+  const { projects } = loaderData
+  console.log(projects);
+  
   return (
     <>
       <h1 className=''>projects</h1>
