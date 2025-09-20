3071d61c80efdc599f83b98fa44cdf8214c62362
kennzuki <kingsyken@gmail.com>
2025-09-20 17:42:38 +0300

    search functionality in the blod index page

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -3,6 +3,7 @@ import type { Route } from './+types/index';
 import PostCard from '~/components/PostCard';
 import { useState } from 'react';
 import Pagination from '~/components/Pagination';
+import PostFilter from '~/components/PostFilter';
 
 
 export async function loader({
@@ -19,21 +20,35 @@ export async function loader({
 }
 const BlogPage = ({ loaderData }: Route.ComponentProps) => {
     const [currentPage, setCurrentPage] = useState(1);
+    const [searchQuery, setSearchQuery] = useState('');
     const postPerPage = 3;
-    const totalPages = Math.ceil(loaderData.posts.length / postPerPage);
+    const { posts } = loaderData;
+    const filteredPosts = posts.filter((post: PostMeta) => {
+        const query = searchQuery.toLowerCase();
+        return post.title.toLowerCase().includes(query)||post.excerpt.toLowerCase().includes(query);
+})
+
+    const totalPages = Math.ceil(filteredPosts.length / postPerPage);
     const indexOfLastPost = currentPage * postPerPage;
     const indexOfFirstPost = indexOfLastPost - postPerPage;
-    const currentPosts = loaderData.posts.slice(indexOfFirstPost, indexOfLastPost);
+    const currentPosts = filteredPosts.slice(indexOfFirstPost, indexOfLastPost);
 
-  const { posts } = loaderData;
 
   return (
     <>
       <div className='max-w-3xl mx-auto mt-10 px-6 py-6 bg-gray-900'>
-        <h2 className='text-3xl text-white font-bold mb-8'>📝 Blog</h2>
-        {currentPosts.map((post) => (
-          <PostCard key={post.slug} post={post} />
-        ))}
+              <h2 className='text-3xl text-white font-bold mb-8'>📝 Blog</h2>
+              <PostFilter searchQuery={searchQuery} onSearchange={(query) => { setSearchQuery(query); setCurrentPage(1); }} />
+              <div className="space-y-8">
+                  {currentPosts.length === 0 ? (<p className="text-gray-400 text-center">No post found</p>
+                  
+                  ) : (
+                      currentPosts.map((post: PostMeta) => (
+                          <PostCard key={post.slug} post={post} />
+                      ))
+                  )}
+              </div>
+       
               {totalPages > 1 && <Pagination totalPages={totalPages} currentPage={currentPage} onPageChange={(page)=>setCurrentPage(page)}/>}
       </div>
     </>
