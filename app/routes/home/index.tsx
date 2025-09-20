4d684d7d9392c3dc049b730aa21fb98ecba3a010
kennzuki <kingsyken@gmail.com>
2025-09-20 21:21:53 +0300

    fetching both post and projects

--- app/routes/home/index.tsx
+++ app/routes/home/index.tsx
@@ -1,4 +1,4 @@
-import type { Project } from '~/types';
+import type { Project, PostMeta } from '~/types';
 import type { Route } from './+types/index';
 import Featured from '~/components/Featured';
 import AboutPreview from '~/components/AboutPreview';
@@ -10,21 +10,36 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-export async function loader({request}:Route.LoaderArgs):Promise<{projects:Project[]}>  {
-  const res=await fetch(`${import.meta.env.VITE_API_URL}/projects`);
-  const data=await res.json();
-  return {projects:data}
+export async function loader({
+  request,
+}: Route.LoaderArgs): Promise<{ projects: Project[];posts: PostMeta[] }> {
+  const [projectRes, postRes] = await Promise.all([
+    fetch(`${import.meta.env.VITE_API_URL}/projects`),
+    fetch(new URL('/post-meta', request.url)),
+  ]);
+
+  if (projectRes.ok || postRes.ok) {
+    throw new Error('Failed to find the data');
+  }
+
+  const [projects, posts] = await Promise.all([
+    projectRes.json(),
+    postRes.json(),
+  ]);
+console.log(projects,posts);
+
+  return {projects,posts };
 }
 
-const HomePage=({loaderData}:Route.ComponentProps)=> {
-  const{projects}=loaderData
-  
+const HomePage = ({ loaderData }: Route.ComponentProps) => {
+  const { projects,posts } = loaderData;
+
   return (
     <section className=''>
       <Featured projects={projects} count={2} />
-      <AboutPreview/>
+      <AboutPreview />
     </section>
   );
-}
+};
 
-export default HomePage
+export default HomePage;
