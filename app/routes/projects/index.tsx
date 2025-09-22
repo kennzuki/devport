8d8a38901a61fd47b04aceb61c108af22dcb333d
kennzuki <kingsyken@gmail.com>
2025-09-22 21:44:52 +0300

    refactoring fetch to switch to strapi api

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -15,9 +15,22 @@ export function meta({}: Route.MetaArgs) {
 export async function loader({
   request,
 }: Route.LoaderArgs): Promise<{ projects: Project[] }> {
-  const res = await fetch(`${import.meta.env.VITE_API_URL}/projects`);
-  const data = await res.json();
-  return { projects: data };
+  const res = await fetch(`${import.meta.env.VITE_API_URL}/projects?populate=*`);
+  const json = await res.json();
+  const projects = json.data.map((item) => ({
+    id: item.id,
+    documentId: item.documentId,
+    title: item.title,
+    description: item.description,
+    image: item.image?.url ? `${item.image.url}` : '/images/no-image.png',
+    url: item.url,
+    date: item.date,
+    category: item.category,
+    featured: item.featured,
+  }));
+  console.log(projects);
+  
+  return { projects };
 }
 
 const ProjectsPage = ({ loaderData }: Route.ComponentProps) => {
